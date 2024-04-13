07580b9022a5832686891c330e7cee3a77d0f525
goncalor21 <goncaloramalho21@gmail.com>
2024-04-13 10:52:53 +0100

    Added services.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,46 +4,79 @@ import CardItem from './CardItem';
 
 function Cards() {
   return (
+
+
+
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
-          </ul>
+      <div class="container">
+        <div class="motto-wrap">
+          <div id="quem-somos" class="label cc-light">quem somos
+          </div>
+          <div class="heading-jumbo-small">
+            Do seu lado, na procura do sucesso no mundo industrial.
+            <br></br>
+          </div>
+        </div>
+        <div class="divider">
+        </div>
+        <div class="home-content-wrap">
+          <div class="w-layout-grid about-grid">
+            <div id="w-node-_86e64837-0616-515b-4568-76c147234d34-6b08ebcb">
+              <div class="home-section-wrap"><div class="label cc-light">sobre nós</div>
+                <h2 class="section-heading">Identidade</h2>
+                <p class="paragraph-light">Fundada em 1990, a M.R. nasceu com o objetivo de
+                  acrescentar valor ao cliente industrial através do fabrico de acessórios,
+                  ferragens, ferramentas e peças técnicas de precisão, rigor e pormenor.
+                </p>
+              </div>
+            </div>
+            <img src="https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min.jpg" id="w-node-_86e64837-0616-515b-4568-76c147234d3f-6b08ebcb" alt="" sizes="94vw" srcset="https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-500.jpg 500w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-800.jpg 800w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-1080.jpg 1080w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-1600.jpg 1600w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-2000.jpg 2000w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-2600.jpg 2600w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min-p-3200.jpg 3200w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451315395a55531e7c189a6_detalhe-de-aco-inoxidavel-para-maquinas-min.jpg 5184w"></img>
+          </div>
+        </div>
+        <div class="w-layout-grid about-grid cc-about-2">
+          <div id="w-node-_86e64837-0616-515b-4568-76c147234d41-6b08ebcb">
+            <div class="home-section-wrap">
+              <div class="label cc-light">o que fazemos
+              </div>
+              <h2 class="section-heading">Áreas de foco</h2>
+              <p class="paragraph-light">Dispomos de serviços de maquinagem, soldadura e
+                estampagem de metais, ligas metálicas, plásticos e poliamidas (entre outros).
+                Execução artigos de pequena e média escala à medida das necessidades do
+                mercado e das indústrias(transportes, aeroespacial, informática e eletrotecnia,
+                sistemas de comunicação, construção civil e &nbsp;
+                produção de energia elétrica).</p>
+            </div>
+          </div>
+          <img src="https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas.jpg" id="w-node-_86e64837-0616-515b-4568-76c147234d4c-6b08ebcb" alt="" sizes="94vw" srcset="https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas-p-500.jpg 500w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas-p-800.jpg 800w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas-p-1080.jpg 1080w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas-p-1600.jpg 1600w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas-p-2000.jpg 2000w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas-p-2600.jpg 2600w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451337692b4ec5e1d7db6fc_trabalhador-trabalhando-com-um-moedor-circular-em-um-metal-com-faiscas.jpg 3000w"></img>
+        </div>
+        <div class="w-layout-grid about-grid-copy">
+          <div id="w-node-_2c670023-ad78-620a-596c-dd545177a41f-6b08ebcb">
+            <div class="home-section-wrap">
+              <div class="label cc-light">dispomos
+              </div>
+              <h2 class="section-heading">O que nos distingue</h2>
+              <p class="paragraph-light">Com o objetivo de colocar o cliente em primeiro lugar,
+                dispomos de:<br></br>Uma grande flexibilidade nos prazos de entrega e na sua capacidade;
+                <br></br>Preços extremamente competitivos e abertos a pedido de cotação para comparação.
+              </p>
+            </div>
+            {/* <a href="/about" class="button w-inline-block">
+                  <div>contacte-nos</div>
+                  </a> */}
+          </div>
+          <img src="https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min.jpg" id="w-node-_2c670023-ad78-620a-596c-dd545177a42a-6b08ebcb" alt="" sizes="94vw" srcset="https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-500.jpg 500w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-800.jpg 800w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-1080.jpg 1080w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-1600.jpg 1600w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-2000.jpg 2000w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-2600.jpg 2600w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min-p-3200.jpg 3200w, https://uploads-ssl.webflow.com/64512d24cf3b711cd608ebc8/6451354c5d0d1b6f8ff0dbe8_processo-de-criacao-de-cerca-moderna-com-faiscas-laranja-min.jpg 6720w"></img>
         </div>
       </div>
+      <div class="services-section">
+      <div class="label cc-light">Nossos serviços</div>
+      <h2 class="section-heading">Serviços que oferecemos</h2>
+      <ul class="services-list">
+        <li>Serviço 1: Descrição do serviço 1.</li>
+        <li>Serviço 2: Descrição do serviço 2.</li>
+        <li>Serviço 3: Descrição do serviço 3.</li>
+        {/* Add more services as needed */}
+      </ul>
+    </div>
     </div>
   );
 }
